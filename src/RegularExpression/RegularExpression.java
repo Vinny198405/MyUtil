51beb3d7f9848d25f2200fd58451e1d4d413e7cd
Vinny198405 <Vinny1984@mail.ru>
2020-04-17 23:27:54 +0300

    HW Regular expression

--- src/RegularExpression/RegularExpression.java
+++ src/RegularExpression/RegularExpression.java
@@ -14,6 +14,7 @@ class RegularExpression {
 
     /**
      * string containing number less than 256
+     *
      * @return regular expression string
      */
     static String numberLess256() {
@@ -21,22 +22,19 @@ class RegularExpression {
         return "\\d|[01]\\d{2}|\\d{2}|2[0-4]\\d|25[0-5]";
     }
 
-    static String ipV4(){
-        return String.format("((%s)\\.){3}(%s)",numberLess256(),numberLess256());
+    static String ipV4() {
+        return String.format("((%s)\\.){3}(%s)", numberLess256(), numberLess256());
     }
 
-   static String phone()
-    {
+    static String phone() {
         return "(\\+972-?|0)5[0|2-8](-?\\d){7}";
     }
 
-    static String email()
-    {
+    static String email() {
         return "[^\\s,@]*@[a-zA-Z0-9]+(-?[a-zA-Z0-9])*(\\.[a-zA-Z](-?[a-zA-Z])*[a-zA-Z]?){1,3}";
     }
 
-    static String arithmeticExpression()
-    {
+    static String arithmeticExpression() {
         return "\\s*([0-9]\\s*[\\+\\-\\*\\/]?\\s*)*;?\\s*";
     }
 }

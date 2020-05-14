457bd58552952540cc513da33db592726a414a25
Vinny198405 <Vinny1984@mail.ru>
2020-05-14 19:42:09 +0300

    HW Anagram

--- src/Anagram/Anagram.java
+++ src/Anagram/Anagram.java
@@ -8,23 +8,28 @@ public class Anagram {
             return false;
         }
 
-        if(word.length() != anagram.length()) {
+        if (word.length() != anagram.length()) {
             return false;
         }
         HashMap<Character, Integer> charCountsMap = getCharCounts(word);
-        for(char c: anagram.toCharArray()) {
-            if (charCountsMap.merge(c, 1, (w, a) -> w - a) < 0) {
+        for (char c : anagram.toCharArray()) {
+//            if (charCountsMap.merge(c, 1, (w, a) -> w - a) < 0) {
+//                return false;
+//            }
+            if (charCountsMap.compute(c, (key, val) -> val - 1) == -1) {
                 return false;
             }
+
         }
         return true;
     }
 
     private static HashMap<Character, Integer> getCharCounts(String word) {
         HashMap<Character, Integer> res = new HashMap<>();
-        for(char c: word.toCharArray()) {
-            Integer count = res.getOrDefault(c, 0);
-            res.put(c, count + 1);
+        for (char c : word.toCharArray()) {
+//            Integer count = res.getOrDefault(c, 0);
+//            res.put(c, count + 1);
+            res.merge(c, 1, Integer::sum);
         }
         return res;
     }

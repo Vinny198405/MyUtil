6bdf07174f90a002dfc38cd5db067e3295a005d7
Vinny198405 <Vinny1984@mail.ru>
2020-07-03 10:21:01 +0300

    HW_48_Racer

--- src/_48_HW_ThreadsRaceSyncWinnerAtomic/Race.java
+++ src/_48_HW_ThreadsRaceSyncWinnerAtomic/Race.java
@@ -3,10 +3,11 @@ package _48_HW_ThreadsRaceSyncWinnerAtomic;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Race {
-    public  int min_sleep;
-    public  int max_sleep;
+    public int min_sleep;
+    public int max_sleep;
     public int distance;
-    public AtomicInteger winnerId;
+    public AtomicInteger winnerId = new AtomicInteger(0);
+
     public Race(int min_sleep, int max_sleep, int distance) {
         super();
         this.min_sleep = min_sleep;
